32bea61d8255177435d953b794b68e13605b9423
Anjali Upadhyay <anjalijodhpur26@gmail.com>
2024-09-11 10:41:09 +0530

    commit 1

---
+++ src/main/java/com/home/demos/PrepInsta_Training/PrepInstaTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.home.demos.PrepInsta_Training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepInstaTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrepInstaTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/home/demos/PrepInsta_Training/PrepInstaTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.demos.PrepInsta_Training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrepInstaTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
